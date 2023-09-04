cd501ef8ff7e2e89907323eea5f5f25859cb4877
Erkko Vallivaara <erkko.vallivaara@gmail.com>
2023-09-04 17:27:59 +0300

    Added add rule, and lowest ja highest rules

--- dice_roller.py
+++ dice_roller.py
@@ -21,4 +21,32 @@ def dice_roll(custom, amount, dice):
     for _ in range(amount):
         dice_results.append(die_roll(dice_sides))
     
-    return dice_results   
+    return dice_results
+
+def add_dice(dice_results):
+    return sum(dice_results)
+
+def dice_sum(dice_results, rules):
+    
+    final_result = {}
+    if ('add' in rules):
+        if ('hgh' in rules):
+            sorted_results = sorted(dice_results, reverse=True)
+            calc_results = sorted_results[:rules['hgh']]
+        elif ('lwt' in rules):
+            sorted_results = sorted(dice_results, reverse=False)
+            calc_results = sorted_results[:rules['lwt']]
+        else:
+            sorted_results = sorted(dice_results, reverse=True)
+            calc_results = sorted_results
+
+        final_result = {'result': add_dice(calc_results), 'dice_results': sorted_results}
+
+    return final_result
+
+print(dice_sum(dice_roll(False, 5, 10), {'add': True, 'hgh': 3}))
+
+# rules = json template?
+# read rules as switch case?
+# dice roll template: "[num]d[roll][rules]"
+        
