b6b2974e74e46c3be98e97b73ea0f31ef6fec06f
Erkko Vallivaara <erkko.vallivaara@gmail.com>
2023-10-23 17:44:05 +0300

    added history and load + saving placeholders

--- main.py
+++ main.py
@@ -2,12 +2,21 @@ from dice_roller import dice_roller
 from roll_input_parser import roll_input_parser
 from guided_roll import guided_roll
 from instructions import (start_instructions, info_instructions)
+from saving import (load_rolls, save_rolls)
 
+roll_history = []
 
 def roll_from_input(roll_input):
     roll = roll_input_parser(roll_input)
     results = dice_roller(*roll)
     print(results)
+    roll_history.append(results)
+
+def guided_rolling():
+    roll = guided_roll()
+    results = dice_roller(*roll)
+    print(results)
+    roll_history.append(results)
 
 round = 0
 
@@ -29,9 +38,19 @@ while True:
             print('Invalid roll syntax')
     elif (user_input == "" or user_input == "guided"):
         try:
-            guided_roll()
+            guided_rolling()
         except:
             print('Invalid guided roll syntax')
+    elif (user_input == 'load'):
+        try:
+            load_rolls(roll_history)
+        except:
+            print('Error in loading roll history')
+    elif (user_input == 'save'):
+        try:
+            save_rolls(roll_history)
+        except:
+            print('Error in loading roll history')
     else:
         print('Invalid command: type "info" for instruction.')
 
---
+++ saving.py
@@ -0,0 +1,7 @@
+def load_rolls(rolls):
+    print('placeholder')
+    for each in rolls:
+        print(each)
+    
+def save_rolls(rolls):
+    print('placeholder')
