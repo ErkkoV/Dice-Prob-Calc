467f8974917ad4e57d9c51b0b69c6f50afb42c2e
Erkko Vallivaara <erkko.vallivaara@gmail.com>
2023-10-30 17:23:39 +0200

    Added info and cleaned comments

--- instructions.py
+++ instructions.py
@@ -9,7 +9,7 @@ def start_instructions():
     "guided:" starts guided roll sequence, also starts guided roll with input "".
     'load': loads currently saved rolls, and shows them. autoload at the start is on by the default
     'save': saves current roll history, autosave is on by the default.
-    'info' for more info.
+    'info' for roll-syntax explation
     
     Make a direct roll by typing roll-syntax:
     "r_dice_amount_d[...dice_sides][rules]"
@@ -21,4 +21,25 @@ def start_instructions():
 
 
 def info_instructions():
-    print("placeholder")
+    print(
+        """
+    roll-syntax explained:
+    
+    r4d[1,2,2,2,2,3][add1,hgh2]
+    r5d10[add1,hgh3]
+
+    r   always starts the roll
+    4   is amount of dice rolled, can be any integer that is higher than 0
+    
+    10  is maximum side of the die. In this case die has sides from 1 to 10.
+        or
+    []  contains a list of custom die sides.
+    
+    []  contains list of rules, rules are always in format:
+            
+          add    three letter rule
+          1      truth value of the rule, or integer if rule needs it.
+          ,      separates rules    
+          
+          """
+    )
